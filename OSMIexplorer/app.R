d204260ab8fb92f1317c2f3185678aa13aa1d56d
homebase3 <65870950+homebase3@users.noreply.github.com>
2021-03-27 13:23:33 -0400

    Clean-up code and file library. Check tour for typos.

--- OSMIexplorer/app.R
+++ OSMIexplorer/app.R
@@ -1,12 +1,9 @@
 
-# load packages
+# load shiny specific packagespackages
 library(shiny)
 library(shinyWidgets)
-library(tidyverse)
-library(kableExtra)
 library(shinythemes)
 library(shinyalert)
-library(cicerone)
 library(rintrojs)
 
 # source
@@ -14,12 +11,12 @@ source("model.R")
 
 # define tour
 tour_list <- list()
-tour_list[['#tour']] <- '<b> Welcome to the tour! </b> <br> Click through for a full introduction of the workflow!'
-tour_list[[paste0("#",gsub(" ","_",names(topics)[1]))]] <- " <b> Select your variables. </b> <br> The OSMI data has over 80 variables. In this step, you'll select the variables that you will chart. Variables are grouped by topic. If you are interested in a given topic, click on it and select the relevant variables."
-tour_list[['#datapoints']] <- 'Remember to check the number of complete datapoints for your selected variables to ensure that there are enough datapoints before you move on.'
+tour_list[['#tour']] <- '<b> Welcome to the tour! </b> <br> Click through for a full introduction of the workflow.'
+tour_list[[paste0("#",gsub(" ","_",names(topics)[1]))]] <- " <b> Select your variables. </b> <br> The OSMI data has over 80 variables. In this step, you'll select the variables that you will chart. Variables are grouped by topic. If you are interested in a given topic, click on it and select the specific variables you are interested in from the menu"
+tour_list[['#datapoints']] <- 'Remember to check the number of complete datapoints for your selected variables to ensure that there are enough datapoints for nontrivial analysis before you move on.'
 tour_list[['#update']] <- "<b> Update your variables in the charting interface. </b> <br> Once you've selected your variables, click this button to migrate your variables to the charting interface. You can always come back and reselect your variables at any time. If you do, just click this button again and they will update."
-tour_list[['#step_3']] <- "<b> Select your X, Y, and Group variables for charting </b> <br> With your smaller sample of variables, select the X, Y, and Group variables you'd like to chart. You only need to choose and X variable and a Y variable or Group Variable to chart. More on this next."
-tour_list[['#step_4']] <- "<b> Select your chart type. </b> <br> Now that you've selected your variables to chart, you can choose from a menu of charting options for these choices by clicking on the blue gear below. The menu updates dynamically based on the types of variables (e.g. categorical, continuous) you have chosen in the prior step. If no variables have been selected or there is a charting error, the chart will appear blank. In case you're curious on more specific info on the variables you've chosen to chart -- like the exact question that was asked, click over to the \"Variable info\" tab anytime. You won't lose your charting progress."
+tour_list[['#step_3']] <- "<b> Organie your variables for charting </b> <br> With your selected set of variables, choose the X, Y, and Group variables you'd like to chart. You only need to choose an X variable and a Y variable or Group variable to chart. More on this next."
+tour_list[['#step_4']] <- "<b> Select your chart type. </b> <br> Now that you've organized your variables for charting, you can choose from a menu of charting options by clicking on the blue gear below. The menu updates dynamically based on the types of variables (e.g. categorical, continuous) you have chosen in the prior step. If no variables have been selected or there is a charting error, the chart will appear blank. And in case you're curious on more specific info on the variables you've chosen to chart -- like the exact question that was asked in the survey, click over to the \"Variable info\" tab anytime. You won't lose your charting progress."
 
 tour_list %>% 
   do.call(rbind,.) %>% 
@@ -60,7 +57,7 @@ ui <- fluidPage(
 
     # build main panel for plots
     mainPanel(
-      h4("Step 3: Select variables for charting",id = "step_3"),
+      h4("Step 3: Organize variables for charting",id = "step_3"),
       lapply(c("X_var", "Y_var", "Group_var"), function(var) {
         div(
           style = "display:inline-block",
@@ -108,7 +105,7 @@ server <- function(input, output, session) {
   # initial alert upon opening app
   shinyalert(
     title = "Welcome to the OSMI explorer",
-    text = "This is explorer is meant to help users make sense of Open Sourcing Mental Illness' survey data on the tech industry from 2019. It allows you to identify questions of interest, find questions with non-sparse responses quickly, and then build visualizations based on variables in the survey that can help you make sense of the data. If this is your first time using the explorer, please click tour on the top left for more information on the workflow."
+    text = "This explorer is meant to help users make sense of Open Sourcing Mental Illness' survey data on the tech industry from 2019. It allows you to identify questions of interest, find questions with non-sparse responses quickly, and then build visualizations based on variables in the survey that can help you make sense of the data. If this is your first time using the explorer, please click the 'tour' button on the top left for more information on the workflow."
   )
 
   # guided app onboarding tour
